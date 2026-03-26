17e64526036e6bb5df9feaeb5feae271c4b7ff64
Thoth <jonathankurtz@protonmail.com>
2026-03-26 09:23:22 -0500

    init basic eval chain files, add no_std

--- crates/axi-core/src/lib.rs
+++ crates/axi-core/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![no_std]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+extern crate alloc;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod evaluator;
+pub mod parser;
+pub mod lexer;
